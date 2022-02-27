a511c7027c9fa0cfe793181c7909d5033ac1f47b
mbakin <mehmetberkakinn@outlook.com>
2022-02-27 18:18:35 +0300

    test: unit testing for handler pkg tests red

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -2,31 +2,40 @@ package handler_test
 
 import (
 	"backend_todo/handler"
+	"backend_todo/mocks"
+	"backend_todo/model"
+	"encoding/json"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestGetTodos(t *testing.T) {
-
-	todoHandler := handler.NewHandlerTodo()
-
-	t.Run("only GET method allowed", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodGet, "/api/v1/todos", http.NoBody)
-		res := httptest.NewRecorder()
-
-		todoHandler.GetTodos(res, req)
-
-		assert.Equal(t, http.StatusOK, res.Result().StatusCode)
+func Test_Handler_GetTodos(t *testing.T) {
+	//repositoryReturn := map[string]*model.Todo{
+	//	"Dummy": &model.Todo{
+	//		ID:   1000,
+	//		Todo: "Dummy",
+	//	},
+	//}
+	expectedRepository := make([]*model.Todo, 0)
+	expectedRepository = append(expectedRepository, &model.Todo{
+		ID:   1000,
+		Todo: "Dummy",
 	})
 
-	t.Run("only POST method allowed", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodPost, "/api/v1/todos", http.NoBody)
-		res := httptest.NewRecorder()
+	service := mocks.NewMockITodoService(gomock.NewController(t))
+	service.EXPECT().GetTodos().Return(expectedRepository).Times(1)
 
-		todoHandler.AddTodo(res, req)
+	handler := handler.NewHandlerTodo(service)
+	r := httptest.NewRequest(http.MethodGet, "/api/v1/todos", nil)
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, r)
 
-		assert.Equal(t, http.StatusOK, res.Result().StatusCode)
-	})
+	actual := w.Body.String()
+	json.Unmarshal(w.Body.Bytes(), &actual)
+
+	assert.Equal(t, w.Result().StatusCode, http.StatusOK)
+	assert.Equal(t, "application/json; charset=utf-8", w.Header().Get("content-type"))
 }

5d9919611a18bada2c39fbf8ba5f07ff22f5db75
mbakin <mehmetberkakinn@outlook.com>
2022-02-27 12:19:44 +0300

    initial mock repository in service_test.go

--- service/service_test.go
+++ service/service_test.go
@@ -1,13 +1,20 @@
 package service_test
 
 import (
+	"backend_todo/mocks"
 	"backend_todo/service"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_TodoService(t *testing.T) {
-	todoService := service.NewTodoService()
+
+	mockController := gomock.NewController(t)
+	defer mockController.Finish()
+	mockTodoRepository := mocks.NewMockIRepositoryTodo(mockController)
+
+	todoService := service.NewTodoService(mockTodoRepository)
 	getTodos := todoService.GetTodos()
 
 	assert.Nil(t, getTodos)
